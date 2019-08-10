9592222aaed79d04848f7213e0d81544a5d76a6c
Brandon Pittman <brandon@brandonpittman.net>
2019-08-10 18:01:04 +0900

    wip

--- build.js
+++ build.js
@@ -8,36 +8,34 @@ const path = require('path')
 
 // Define options for Boxen
 const options = {
-  padding: 1,
-  margin: 1,
-  borderStyle: 'round'
+	padding: 1,
+	margin: 1,
+	borderStyle: 'round',
 }
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('              Brandon Pittman'),
-  handle: chalk.white('brandonpittman'),
-  work: chalk.white('Director @ Aquaring Global Strategy'),
-  opensource:
-    chalk.white('Gridsome | Tailwind | PurgeCSS ') +
-    chalk.green('⬢'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('brandonpittman'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~brandonpittman'),
-  github: chalk.gray('https://github.com/') + chalk.green('brandonpittman'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
-  wantedly:
-    chalk.gray('https://www.wantedly.com/users/') + chalk.blue('80873258'),
-  web: chalk.cyan('https://blp.is/here'),
-  npx: chalk.red('npx') + ' ' + chalk.white('brandonpittman'),
-  labelWork: chalk.white.bold('       Work:'),
-  labelOpenSource: chalk.white.bold('Open Source:'),
-  labelTwitter: chalk.white.bold('    Twitter:'),
-  labelnpm: chalk.white.bold('        npm:'),
-  labelGitHub: chalk.white.bold('     GitHub:'),
-  //  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-  labelWantedly: chalk.white.bold('   Wantedly:'),
-  labelWeb: chalk.white.bold('        Web:'),
-  labelCard: chalk.white.bold('       Card:')
+	name: chalk.white('              Brandon Pittman'),
+	handle: chalk.white('brandonpittman'),
+	work: chalk.white('Technical Director @ Aquaring Global Strategy'),
+	opensource: chalk.white('Gridsome | Tailwind | PurgeCSS ') + chalk.green('⬢'),
+	twitter: chalk.gray('https://twitter.com/') + chalk.cyan('brandonpittman'),
+	npm: chalk.gray('https://npmjs.com/') + chalk.red('~brandonpittman'),
+	github: chalk.gray('https://github.com/') + chalk.green('brandonpittman'),
+	linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
+	wantedly:
+		chalk.gray('https://www.wantedly.com/users/') + chalk.blue('80873258'),
+	web: chalk.cyan('https://blp.is/here'),
+	npx: chalk.red('npx') + ' ' + chalk.white('brandonpittman'),
+	labelWork: chalk.white.bold('       Work:'),
+	labelOpenSource: chalk.white.bold('Open Source:'),
+	labelTwitter: chalk.white.bold('    Twitter:'),
+	labelnpm: chalk.white.bold('        npm:'),
+	labelGitHub: chalk.white.bold('     GitHub:'),
+	//  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
+	labelWantedly: chalk.white.bold('   Wantedly:'),
+	labelWeb: chalk.white.bold('        Web:'),
+	labelCard: chalk.white.bold('       Card:'),
 }
 
 // Actual strings we're going to output
@@ -55,29 +53,29 @@ const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
 const output =
-  heading +
-  newline +
-  newline +
-  working +
-  newline +
-  opensourcing +
-  newline +
-  newline +
-  twittering +
-  newline +
-  npming +
-  newline +
-  githubing +
-  newline +
-  wantedlying +
-  newline +
-  newline +
-  webing +
-  newline +
-  newline +
-  carding
+	heading +
+	newline +
+	newline +
+	working +
+	newline +
+	opensourcing +
+	newline +
+	newline +
+	twittering +
+	newline +
+	npming +
+	newline +
+	githubing +
+	newline +
+	wantedlying +
+	newline +
+	newline +
+	webing +
+	newline +
+	newline +
+	carding
 
 fs.writeFileSync(
-  path.join(__dirname, 'bin/output'),
-  chalk.green(boxen(output, options))
+	path.join(__dirname, 'bin/output'),
+	chalk.green(boxen(output, options)),
 )
